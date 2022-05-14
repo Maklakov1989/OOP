03d87b79d1ada25e3325a0fd6014881cb67e5a38
Alexey <maklakov_a.n@mail.ru>
2022-05-14 12:10:07 +0300

    Created

--- E-mail.py
+++ E-mail.py
@@ -7,7 +7,7 @@ words = re.findall('\w{4,100}', text)
 new_list = [el for el in words if words.count(el) < 10]
 pattern1 = re.compile('[\w+./]+[a-z]+[$ .\w]')
 link = pattern1.findall(text)
-link2 = re.findall('[^. ]+\w+[.]+\w+[/]',text)
+link2 = re.findall('[^. ]+\w+[.]+\w+[/ .]',text)
 print(link2)
 
 
