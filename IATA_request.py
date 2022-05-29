37ef67b3cd70f06a999bf052dc0b8fb9cd2a0c0f
Alexey <maklakov_a.n@mail.ru>
2022-05-29 12:13:45 +0300

    Created

---
+++ IATA_request.py
@@ -0,0 +1,12 @@
+import requests
+import json
+origin = input("Вы хотите вылететь из:")
+destination = input("Введите город прибытия:")
+link = "https://www.travelpayouts.com/widgets_suggest_params?q=Из%20"+origin+"%20в%20"+destination
+text = requests.get(link).text
+data = json.loads(text)
+iata_origin = data["origin"]["iata"]
+iata_destination = data['destination']['iata']
+print(f'IATA код {origin} -', iata_origin)
+print(f'IATA код {destination} -', iata_destination)
+
